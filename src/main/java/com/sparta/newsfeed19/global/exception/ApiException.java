8cdf8d4cc6a5a5d3a06ced862bb2871407dc2a12
kim-na-ram <snrneh3@naver.com>
2024-09-02 20:28:28 +0900

    [FEAT] exception 처리 추가

---
+++ src/main/java/com/sparta/newsfeed19/global/exception/ApiException.java
@@ -0,0 +1,16 @@
+package com.sparta.newsfeed19.global.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class ApiException extends RuntimeException {
+    private final String message;
+    private final HttpStatus code;
+
+    public ApiException(ResponseCode responseCode){
+        super("[" + responseCode.getCode() + "] " + responseCode.getMessage());
+        this.message = responseCode.getMessage();
+        this.code = responseCode.getCode();
+    }
+}
